0365efb6736420d1539574a768efbd399923f80d
Robert Higgins <rthiggins@siu.edu>
2020-06-09 22:02:59 -0500

    Added random seed to function

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"context"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 
 	runtime "github.com/aws/aws-lambda-go/lambda"
+	"github.com/wandersoulz/godes"
 	"github.com/wandersoulz/randomname"
 )
 
+func generateRandomSeed() int64 {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 1337227
+	return (int64)(rand.Intn(max-min+1) + min)
+}
+
 func init() {
+	godes.SetSeed(generateRandomSeed())
 	contextSizeStr := os.Getenv("CONTEXT_SIZE")
 	contextSize, _ := strconv.Atoi(contextSizeStr)
 	randomname.Init("first-names.txt", contextSize)
